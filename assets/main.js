829a1000b9605139a1c1cd4de3378a2f4614a6e9
Noreras <alex22030192@mail.ru>
2026-02-11 17:19:52 +0300

    Delete assets/main.js

--- assets/main.js
+++
@@ -1,8 +0,0 @@
-// Точка входа - инициализация игры
-
-document.addEventListener('DOMContentLoaded', () => {
-    const board = new GameBoard(GameConfig);
-    const animations = new GameAnimations(GameConfig);
-    const ui = new GameUI(GameConfig);
-    const game = new Match3Game(GameConfig, board, animations, ui);
-});
